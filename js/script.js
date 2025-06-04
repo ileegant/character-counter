aed460300ed938102228d5abdf163953e939947d
Sukhoruchko Oleh <osukhoruchko@gmail.com>
2025-06-04 10:52:32 +0300

    Add word counter function

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 const textArea        = document.getElementById('text-area');
 const totalCharacters = document.getElementById('total-characters');
+const wordCount       = document.getElementById('word-count');
 
 textArea.addEventListener('input', () => {
-  totalCharacters.textContent = textArea.value.length;
+  totalCharacters.textContent = textArea.value === "" ? "00" : formatCount(textArea.value.length);
+  wordCount.textContent       = textArea.value.trim() === "" ? "00" : formatCount(textArea.value.trim().split(" ").length);
+  console.log(textArea.value.split(" "));
 });
+
+function formatCount(count) {
+  return count.toString().padStart(2, "0");
+}
