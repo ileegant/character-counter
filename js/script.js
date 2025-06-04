db017e748ec35f4f658aa65517ad7465991ec111
Sukhoruchko Oleh <osukhoruchko@gmail.com>
2025-06-04 11:07:55 +0300

    Add sentence count function

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
 const textArea        = document.getElementById('text-area');
 const totalCharacters = document.getElementById('total-characters');
 const wordCount       = document.getElementById('word-count');
+const sentenceCount   = document.getElementById('sentence-count');
 
 textArea.addEventListener('input', () => {
   totalCharacters.textContent = textArea.value === "" ? "00" : formatCount(textArea.value.length);
   wordCount.textContent       = textArea.value.trim() === "" ? "00" : formatCount(textArea.value.trim().split(" ").length);
-  console.log(textArea.value.split(" "));
+  sentenceCount.textContent   = textArea.value.trim() === "" ? "00" : formatCount(removeEmptyLines(textArea.value.trim().split(".")).length);
 });
 
 function formatCount(count) {
   return count.toString().padStart(2, "0");
+}
+
+function removeEmptyLines(arr) {
+  return arr.filter(line => line.trim() !== "");
 }
