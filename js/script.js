8ceac76337cf7432616645d9780517c9052b6a44
Sukhoruchko Oleh <osukhoruchko@gmail.com>
2025-06-04 11:49:50 +0300

    Add functionality for exclude spaces in total counter

--- js/script.js
+++ js/script.js
@@ -2,12 +2,29 @@ const textArea        = document.getElementById('text-area');
 const totalCharacters = document.getElementById('total-characters');
 const wordCount       = document.getElementById('word-count');
 const sentenceCount   = document.getElementById('sentence-count');
+const readingTime     = document.getElementById('reading-time');
+const excludeSpaces   = document.getElementById('exclude-spaces');
 
 textArea.addEventListener('input', () => {
-  totalCharacters.textContent = textArea.value === "" ? "00" : formatCount(textArea.value.length);
-  wordCount.textContent       = textArea.value.trim() === "" ? "00" : formatCount(textArea.value.trim().split(" ").length);
-  sentenceCount.textContent   = textArea.value.trim() === "" ? "00" : formatCount(removeEmptyLines(textArea.value.trim().split(".")).length);
+  let textAreaValue = textArea.value
+
+  updateTotalChar();
+  wordCount.textContent       = textAreaValue.trim() === "" ? "00" : formatCount(textAreaValue.trim().split(" ").length);
+  sentenceCount.textContent   = textAreaValue.trim() === "" ? "00" : formatCount(removeEmptyLines(textAreaValue.trim().split(".")).length);
+  sentenceCount.readingTime   = getReadingTime(100)
 });
+
+excludeSpaces.addEventListener('change', () => {
+  updateTotalChar();
+});
+
+function updateTotalChar() {
+  let charCount = excludeSpaces.checked ?
+      textArea.value.replace(/\s+/g, '').length :
+      textArea.value.length;
+
+  totalCharacters.textContent = formatCount(charCount)
+}
 
 function formatCount(count) {
   return count.toString().padStart(2, "0");
@@ -15,4 +32,8 @@ function formatCount(count) {
 
 function removeEmptyLines(arr) {
   return arr.filter(line => line.trim() !== "");
+}
+
+function getReadingTime(wordCount, wordsPerMinute = 200) {
+
 }
