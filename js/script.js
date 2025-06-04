50f3a07dae109bc07aad68cf049f35e604261038
Sukhoruchko Oleh <osukhoruchko@gmail.com>
2025-06-04 12:47:53 +0300

    Add dark mode

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ const wordCount       = document.getElementById('word-count');
 const sentenceCount   = document.getElementById('sentence-count');
 const readingTime     = document.getElementById('reading-time');
 const excludeSpaces   = document.getElementById('exclude-spaces');
+const themeToggle     = document.getElementById('themeToggle');
+
 
 textArea.addEventListener('input', () => {
   let textAreaValue = textArea.value
@@ -11,11 +13,15 @@ textArea.addEventListener('input', () => {
   updateTotalChar();
   wordCount.textContent       = textAreaValue.trim() === "" ? "00" : formatCount(textAreaValue.trim().split(" ").length);
   sentenceCount.textContent   = textAreaValue.trim() === "" ? "00" : formatCount(removeEmptyLines(textAreaValue.trim().split(".")).length);
-  readingTime.textContent     = getReadingTime()
+  readingTime.textContent     = formatReadingTime(getReadingTime())
 });
 
 excludeSpaces.addEventListener('change', () => {
   updateTotalChar();
+});
+
+themeToggle.addEventListener('change', () => {
+  document.body.classList.toggle('dark', themeToggle.checked);
 });
 
 function getTotalChar() {
@@ -38,4 +44,14 @@ function removeEmptyLines(arr) {
 
 function getReadingTime(wordsPerMinute = 200) {
   return (textArea.value.replace(/\s+/g, '').length * 1.0 / wordsPerMinute).toFixed(1)
+}
+
+function formatReadingTime(readingTime) {
+  let hours = Math.floor(Number(readingTime/60));
+  if (hours === 0) {
+    return readingTime;
+  } else {
+    let minutes = (readingTime - hours * 60).toFixed(1);
+    return `${hours} hours ${minutes}`;
+  }
 }
